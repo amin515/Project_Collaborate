7185b2262f78fbdfc78fd528ec98b2afe16a9d71
dev-nuramin <stack.developer51@gmail.com>
2022-08-16 17:25:56 +0600

    nuramin

--- api/Config/db.js
+++ api/Config/db.js
@@ -6,6 +6,7 @@ const connectmongoDB = async () => {
 
     try{
       
+
         let connect = await mongoose.connect(process.env.MONGO_STRING)
         console.log(`MongoDB connect with Host ${connect.connection.host}`.bgCyan.black)
     }catch(error){
@@ -13,3 +14,4 @@ const connectmongoDB = async () => {
     }
 }
 export default connectmongoDB;
+//console.log

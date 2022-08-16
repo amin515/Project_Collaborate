5decc0cfc1bfd4e2999d1dd15ecc032eb4431270
dev-nuramin <stack.developer51@gmail.com>
2022-08-16 15:57:55 +0600

    initial changes

---
+++ api/Config/db.js
@@ -0,0 +1,15 @@
+
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
+const connectmongoDB = async () => {
+
+    try{
+      
+        let connect = await mongoose.connect(process.env.MONGO_STRING)
+        console.log(`MongoDB connect with Host ${connect.connection.host}`.bgCyan.black)
+    }catch(error){
+        console.log(error)
+    }
+}
+export default connectmongoDB;
